487cf2587b4a157a37ad4b1402f996f3241f71d4
Marija Stanic <marija.stanic@hotmail.ca>
2020-05-05 18:07:14 +0100

    Refactor error handling

--- controllers/feed.js
+++ controllers/feed.js
@@ -8,9 +8,7 @@ exports.getPosts = (req, res, next) => {
       res.status(200).json({ posts: posts });
     })
     .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
+      errorHandler(err);
       next(err);
     });
 };
@@ -39,9 +37,13 @@ exports.createPost = (req, res, next) => {
       });
     })
     .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-        next(err);
-      }
+      errorHandler(err);
+      next(err);
     });
+}; 
+
+const errorHandler = (err) => {
+  if (!err.statusCode) {
+    err.statusCode = 500;
+  }
 };
